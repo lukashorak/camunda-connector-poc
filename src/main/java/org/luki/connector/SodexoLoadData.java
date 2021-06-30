b875b1c1e61c11d1692988ca963644adfbad904b
Lukas Horak <lukas.horak@gmail.com>
2021-06-30 18:43:13 +0200

    Update SodexoLoadData.java

--- src/main/java/org/luki/connector/SodexoLoadData.java
+++ src/main/java/org/luki/connector/SodexoLoadData.java
@@ -1,14 +1,11 @@
 package org.luki.connector;
 
-import org.apache.commons.compress.utils.Lists;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Logger;
 
 @Component
